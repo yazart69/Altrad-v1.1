40f13be78b41618499f11e4ea7758c1c0bcd8ff1
yazart69 <99214459+yazart69@users.noreply.github.com>
2026-02-08 17:06:48 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -7,33 +7,50 @@ import MiddleTiles from "@/components/MiddleTiles";
 
 export default function Home() {
   return (
-    <div className="h-full w-full">
+    <div className="h-full w-full p-2">
       
-      {/* Grille principale du Dashboard */}
-      <div className="grid grid-cols-12 grid-rows-12 gap-6 h-full p-2">
-        
-        {/* LIGNE 1 : KPI & ALERTES */}
-        <div className="col-span-12 lg:col-span-3 row-span-4">
-          <TeamTile />
-        </div>
-        <div className="col-span-12 lg:col-span-6 row-span-2 grid grid-cols-12 gap-6">
-           <MiddleTiles />
-        </div>
-        <div className="col-span-12 lg:col-span-3 row-span-4">
-          <LeavesTile />
+      {/* GRILLE PRINCIPALE (12 Colonnes) */}
+      <div className="grid grid-cols-12 gap-6 h-full">
+
+        {/* --- COLONNE 1 : ÉQUIPES (Gauche - 3 cols) --- */}
+        <div className="col-span-12 xl:col-span-3 flex flex-col gap-6 h-full">
+          {/* Le Donut en haut */}
+          <div className="h-[35%] min-h-[200px]">
+            <TeamTile />
+          </div>
+          {/* La liste des absents en dessous */}
+          <div className="h-[65%] min-h-[300px]">
+            <LeavesTile />
+          </div>
         </div>
 
-        {/* LIGNE 2 : SUIVI CHANTIERS */}
-        <div className="col-span-12 lg:col-span-6 row-span-5">
-          <BudgetHeuresTile />
+
+        {/* --- COLONNE 2 : CENTRAL (Milieu - 6 cols) --- */}
+        <div className="col-span-12 xl:col-span-6 flex flex-col gap-6 h-full">
+            
+            {/* Zone Alertes (Météo/Stock/Notifs) */}
+            {/* On crée une sous-grille de 12 pour que MiddleTiles (qui utilise col-span-4) s'alignent parfaitement sur une ligne */}
+            <div className="grid grid-cols-12 gap-4 min-h-[180px]">
+                <MiddleTiles />
+            </div>
+
+            {/* La Grosse Liste des Chantiers */}
+            <div className="flex-1 min-h-[400px]">
+                <BudgetHeuresTile />
+            </div>
         </div>
 
-        {/* LIGNE 3 : TÂCHES & CHARGE */}
-        <div className="col-span-12 lg:col-span-3 row-span-5">
-           <TasksTile />
-        </div>
-        <div className="col-span-12 lg:col-span-3 row-span-5">
-           <WorkloadTile />
+
+        {/* --- COLONNE 3 : ACTIONS & STATS (Droite - 3 cols) --- */}
+        <div className="col-span-12 xl:col-span-3 flex flex-col gap-6 h-full">
+          {/* Tâches prioritaires (Important donc en haut) */}
+          <div className="h-[60%] min-h-[300px]">
+             <TasksTile />
+          </div>
+          {/* Charge de travail en bas */}
+          <div className="h-[40%] min-h-[200px]">
+             <WorkloadTile />
+          </div>
         </div>
 
       </div>
