c3e54f6b82aac5672ba271f4b39104af26685b88
yazart69 <99214459+yazart69@users.noreply.github.com>
2026-02-10 16:20:26 +0100

    Create page.tsx

---
+++ app/rapports/page.tsx
@@ -0,0 +1,8 @@
+export default function Rapports() {
+  return (
+    <div className="p-10 text-center font-['Fredoka']">
+      <h1 className="text-2xl font-black text-gray-300 uppercase">Rapports</h1>
+      <p className="text-gray-400">Historique et exports PDF (À venir)</p>
+    </div>
+  );
+}
