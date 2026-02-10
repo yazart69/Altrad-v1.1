b1f7b588e3430f15f8e777d1c88e92982af73b55
yazart69 <99214459+yazart69@users.noreply.github.com>
2026-02-10 16:19:54 +0100

    Create page.tsx

---
+++ app/parametres/page.tsx
@@ -0,0 +1,8 @@
+export default function Parametres() {
+  return (
+    <div className="p-10 text-center font-['Fredoka']">
+      <h1 className="text-2xl font-black text-gray-300 uppercase">Paramètres</h1>
+      <p className="text-gray-400">Configuration de l'application (À venir)</p>
+    </div>
+  );
+}
