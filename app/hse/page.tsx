f8c211d569efede4925f989cac00c081088b8d04
yazart69 <99214459+yazart69@users.noreply.github.com>
2026-02-10 16:21:11 +0100

    Create page.tsx

---
+++ app/hse/page.tsx
@@ -0,0 +1,8 @@
+export default function HSE() {
+  return (
+    <div className="p-10 text-center font-['Fredoka']">
+      <h1 className="text-2xl font-black text-gray-300 uppercase">HSE & Sécurité</h1>
+      <p className="text-gray-400">Suivi des incidents et conformité TKT CA ARRIVE LOU (À venir)</p>
+    </div>
+  );
+}
