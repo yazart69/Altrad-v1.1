4bc448333929b6fd927860142f365781a585defd
yazart69 <99214459+yazart69@users.noreply.github.com>
2026-02-12 16:58:01 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ import TasksTile from "@/components/TasksTile";
 import StaffingTile from "@/components/StaffingTile"; 
 import MiddleTiles from "@/components/MiddleTiles";
 import HSETile from "@/components/HSETile"; 
-import PointageTile from "@/components/PointageTile"; // AJOUT IMPORT
+import PointageTile from "@/components/PointageTile"; 
 
 export default function Home() {
   const [stats, setStats] = useState({ staff: 0, alerts: 0, activeSites: 0 });
@@ -33,76 +33,74 @@ export default function Home() {
   }, []);
 
   return (
-    <div className="flex h-screen bg-[#f0f3f4] font-['Fredoka'] overflow-hidden">
+    <div className="font-['Fredoka'] pb-4">
       
-      {/* ZONE DE CONTENU PRINCIPALE */}
-      <main className="flex-1 overflow-y-auto p-4 md:p-8">
-        
-        {/* CONTENEUR GLOBAL FLEX POUR LA STRUCTURE VERTICALE */}
-        <div className="flex flex-col gap-6 pb-10">
+      {/* CONTENEUR RESPONSIVE : 
+          - Mobile : Flex Vertical (col)
+          - Desktop : Grid complexe
+      */}
+      <div className="flex flex-col xl:grid xl:grid-cols-12 gap-6">
 
-          {/* === BLOC SUPÉRIEUR (LIGNE 1 + LIGNE 2 + COLONNE ACTIONS) === */}
-          <div className="grid grid-cols-12 gap-6">
-
-            {/* PARTIE GAUCHE (RH, LOGISTIQUE, BUDGET) - SPAN 9 */}
-            <div className="col-span-12 xl:col-span-9 flex flex-col gap-6">
-              
-              {/* LIGNE 1 : RH & LOGISTIQUE (320px) */}
-              <div className="grid grid-cols-12 gap-6 h-[320px]">
-                <div className="col-span-12 md:col-span-4 h-full">
-                  <LeavesTile />
-                </div>
-                <div className="col-span-12 md:col-span-8 h-full">
-                  <MiddleTiles alertsCount={stats.alerts} /> 
-                </div>
-              </div>
-
-              {/* LIGNE 2 : BUDGET (400px) */}
-              <div className="w-full h-[400px]">
-                <BudgetHeuresTile />
-              </div>
-
+        {/* --- BLOC PRINCIPAL (GAUCHE sur PC, HAUT sur Mobile) --- */}
+        <div className="xl:col-span-9 flex flex-col gap-6">
+          
+          {/* LIGNE 1 : RH & LOGISTIQUE */}
+          {/* Mobile : Stack vertical | Tablette : Grid 2 cols | PC : Grid 12 cols */}
+          <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-12 gap-6 xl:h-[320px]">
+            
+            {/* LeavesTile (Congés) */}
+            <div className="xl:col-span-4 h-[300px] md:h-full">
+              <LeavesTile />
             </div>
-
-            {/* PARTIE DROITE (ACTIONS PRIORITAIRES) - SPAN 3 */}
-            {/* HAUTEUR CALCULÉE : 320px + 400px + 24px (gap) = 744px */}
-            {/* S'arrête exactement au bas de la tuile Budget */}
-            <div className="col-span-12 xl:col-span-3 h-[744px]"> 
-              <TasksTile />
+            
+            {/* MiddleTiles (Matériel/Alertes) */}
+            <div className="xl:col-span-8 h-[300px] md:h-full">
+              <MiddleTiles alertsCount={stats.alerts} /> 
             </div>
-
           </div>
 
-
-          {/* === BLOC INFÉRIEUR (LIGNE 3 : OPÉRATIONS TERRAIN) === */}
-          {/* Reprend toute la largeur et sa hauteur standard */}
-          <div className="grid grid-cols-12 gap-6 h-[350px]">
-            
-            {/* Staffing (4 cols) */}
-            <div className="col-span-12 md:col-span-4 h-full">
-              <StaffingTile staffCount={stats.staff} />
-            </div>
-
-            {/* Répartition (Divisée en 2) (2 cols) */}
-            <div className="col-span-12 md:col-span-2 h-full flex flex-col gap-4">
-              <div className="flex-1 overflow-hidden">
-                  <TeamTile isCompact={true} />
-              </div>
-              <div className="flex-1 overflow-hidden">
-                  {/* REMPLACEMENT ICI : MODULE POINTAGE */}
-                  <PointageTile />
-              </div>
-            </div>
-
-            {/* HSE (6 cols) */}
-            <div className="col-span-12 md:col-span-6 h-full">
-              <HSETile />
-            </div>
-
+          {/* LIGNE 2 : BUDGET */}
+          <div className="w-full h-[350px] md:h-[400px]">
+            <BudgetHeuresTile />
           </div>
 
         </div>
-      </main>
+
+        {/* --- BLOC SECONDAIRE (DROITE sur PC, MILIEU sur Mobile) --- */}
+        {/* Tasks (Tâches Prioritaires) */}
+        <div className="xl:col-span-3 h-[400px] xl:h-[744px]"> 
+          <TasksTile />
+        </div>
+
+      </div>
+
+
+      {/* --- BLOC INFÉRIEUR (OPÉRATIONS) --- */}
+      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-12 gap-6 mt-6 xl:h-[350px]">
+        
+        {/* Staffing */}
+        <div className="xl:col-span-4 h-[300px] xl:h-full">
+          <StaffingTile staffCount={stats.staff} />
+        </div>
+
+        {/* Répartition & Pointage (Empilés sur mobile/PC, côte à côte sur tablette ?) */}
+        {/* Ici on garde l'empilement vertical interne pour la cohérence */}
+        <div className="xl:col-span-2 h-full flex flex-col gap-4 min-h-[300px]">
+          <div className="flex-1 overflow-hidden h-[140px] xl:h-auto">
+              <TeamTile isCompact={true} />
+          </div>
+          <div className="flex-1 overflow-hidden h-[140px] xl:h-auto">
+              <PointageTile />
+          </div>
+        </div>
+
+        {/* HSE (Sécurité) */}
+        <div className="xl:col-span-6 h-[300px] xl:h-full">
+          <HSETile />
+        </div>
+
+      </div>
+
     </div>
   );
 }
