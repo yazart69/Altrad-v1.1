d1a58efdf64502afbfe050c7989da6f64f6cb174
yazart69 <99214459+yazart69@users.noreply.github.com>
2026-02-14 12:21:54 +0100

    Update page.tsx

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,1 +1,15 @@
+"use client";
+
+import React from 'react';
+import AdminDashboard from '@/components/AdminDashboard';
+import AdminPayroll from '@/components/AdminPayroll';
+
+export default function AdminPage() {
+  return (
+    <div className="space-y-8 p-6 font-['Fredoka']">
+      <AdminDashboard />
+      <AdminPayroll />
+    </div>
+  );
+}
 
